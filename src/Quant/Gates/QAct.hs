317f91e7ef91ddbb55dbf78c1eeec3f1aa0be04b
Anomalocaris <flavioborinjunior@gmail.com>
2024-10-25 17:14:59 -0300

    first state monad implemented

--- src/Quant/Gates/QAct.hs
+++ src/Quant/Gates/QAct.hs
@@ -3,24 +3,31 @@ module Gates.QAct
   , runQ
   , app
   , actQop
+  , orc
   ) where
 
 import           Control.Monad.Reader
+import           Control.Monad.State
+import           Control.Monad
 import           Core.Value
 
 type QAct' :: [Natural] -> Natural -> Type -> Type
-type QAct' acs t a = ReaderT (Virt Bit acs t) IO a
+type QAct' acs t a = StateT Int (ReaderT (Virt Bit acs t) IO) a
+
 
 type QAct :: [Natural] -> Natural -> Type
 type QAct acs t = QAct' acs t ()
 
 runQ :: QAct (CountTo t) t -> Virt Bit (CountTo t) t -> IO ()
-runQ = runReaderT
+runQ qa = void . runReaderT (runStateT qa 0)
+
+orc :: QAct (CountTo t) t -> Virt Bit (CountTo t) t -> IO Int
+orc qa val = snd <$> runReaderT (runStateT qa 0) val
 
 actQop :: KnownNat s => Qop Bit (Length acs) (Length acs) -> QAct acs s
 actQop f' = do
   vv <- ask
-  lift $ appV f' vv
+  liftIO $ appV f' vv
 
 app ::
      ValidSelector newacs acs
@@ -30,4 +37,6 @@ app ::
 app sl act = do
   qv <- ask
   let adapterQv = selectQ sl qv
-  lift $ runReaderT act adapterQv
+  liftIO $ runQ' act adapterQv
+    where 
+      runQ' qa = void . runReaderT (runStateT qa 0)
